4b3e68db095de6426fa141056312da07567c401b
Charles Max Crandall <robloxatme.com@gmail.com>
2019-12-04 13:50:08 -0500

    Removed h1 in HTML Added styles to work with the entities being displayed for the game Modified the test level to use ` instead of " Changed ent to entities and changed some of them to be used as classes Created a level constructor that reads in strings of text to use for the game Added states to remember the state of the game Added Vec to utilize a coordinate system Added the classes of each entity(Player, Enemy, Exit) Utilized the elt function to add html elements Added DOMDisplay class Added drawGrid function Added drawActors function Current page will display the test level

--- script.js
+++ script.js
@@ -1,11 +1,178 @@
-let testLevel = "
+ let testLevel = `
 #########
 #@..#%.^#
 #.......#
 #...#..%#
-#########"
-let ent = {
-  "." : "empty" , "#" : "wall" ,
-  "@" : "player", "%" : "enemy" ,
-  "^" : "exit"
-}
+#########`;
+
+ var Level = class Level {
+   constructor(plan) {
+     let rows = plan.trim().split("\n").map(l => [...l]);
+     this.height = rows.length;
+     this.width = rows[0].length;
+     this.startActors = [];
+
+     this.rows = rows.map((row, y) => {
+       return row.map((ch, x) => {
+         let type = entities[ch];
+         if (typeof type == "string") return type;
+         this.startActors.push(
+           type.create(new Vec(x, y), ch));
+         return "empty";
+       });
+     });
+   }
+ }
+
+ var State = class State {
+   constructor(level, actors, status) {
+     this.level = level;
+     this.actors = actors;
+     this.status = status;
+   }
+
+   static start(level) {
+     return new State(level, level.startActors, "playing");
+   }
+
+   get player() {
+     return this.actors.find(a => a.type == "player");
+   }
+ }
+
+ var Vec = class Vec {
+   constructor(x, y) {
+     this.x = x;
+     this.y = y;
+   }
+   plus(other) {
+     return new Vec(this.x + other.x, this.y + other.y);
+   }
+   times(factor) {
+     return new Vec(this.x * factor, this.y * factor);
+   }
+ }
+
+ var Player = class Player {
+   constructor(pos) {
+     this.pos = pos;
+   }
+
+   get type() {
+     return "player";
+   }
+
+   static create(pos) {
+     return new Player(pos.plus(new Vec(0, 0)),
+       new Vec(0, 0));
+   }
+ }
+
+ Player.prototype.size = new Vec(1, 1);
+
+ var Enemy = class Enemy {
+   constructor(pos) {
+     this.pos = pos;
+   }
+
+   get type() {
+     return "enemy";
+   }
+
+   static create(pos) {
+     return new Enemy(pos.plus(new Vec(0, 0)),
+       new Vec(0, 0));
+   }
+ }
+
+ Enemy.prototype.size = new Vec(1, 1);
+
+ var Exit = class Exit {
+   constructor(pos) {
+     this.pos = pos;
+   }
+
+   get type() {
+     return "exit";
+   }
+
+   static create(pos) {
+     return new Exit(pos.plus(new Vec(0, 0)),
+       new Vec(0, 0));
+   }
+ }
+
+ Exit.prototype.size = new Vec(1, 1);
+
+ var entities = {
+   ".": "empty",
+   "#": "wall",
+   "%": Enemy,
+   "@": Player,
+   "^": Exit
+ };
+
+ function elt(name, attrs, ...children) {
+   let dom = document.createElement(name);
+   for (let attr of Object.keys(attrs)) {
+     dom.setAttribute(attr, attrs[attr]);
+   }
+   for (let child of children) {
+     dom.appendChild(child);
+   }
+   return dom;
+ }
+
+ class DOMDisplay {
+   constructor(parent, level) {
+     this.dom = elt("div", {
+       class: "game"
+     }, drawGrid(level));
+     this.actorLayer = null;
+     parent.appendChild(this.dom);
+   }
+
+   clear() {
+     this.dom.remove();
+   }
+ }
+
+ const scale = 100;
+
+ function drawGrid(level) {
+   return elt("table", {
+     class: "background",
+     style: `width: ${level.width * scale}px`
+   }, ...level.rows.map(row =>
+     elt("tr", {
+         style: `height: ${scale}px`
+       },
+       ...row.map(type => elt("td", {
+         class: type
+       })))
+   ));
+ }
+
+ function drawActors(actors) {
+   return elt("div", {}, ...actors.map(actor => {
+     let rect = elt("div", {
+       class: `actor ${actor.type}`
+     });
+     rect.style.width = `${actor.size.x * scale}px`;
+     rect.style.height = `${actor.size.y * scale}px`;
+     rect.style.left = `${actor.pos.x * scale}px`;
+     rect.style.top = `${actor.pos.y * scale}px`;
+     return rect;
+   }));
+ }
+
+ DOMDisplay.prototype.syncState = function(state) {
+   if (this.actorLayer) this.actorLayer.remove();
+   this.actorLayer = drawActors(state.actors);
+   this.dom.appendChild(this.actorLayer);
+   this.dom.className = `game ${state.status}`;
+ };
+
+ let simpleLevel = new Level(testLevel);
+ let display = new DOMDisplay(document.body, simpleLevel);
+ display.syncState(State.start(simpleLevel));
